95a15f97dfa49cf97ba70746e7ef4dcbd1da6838
Andrei Jakupson <ajakupson.job@gmail.com>
2024-10-08 13:32:09 +0300

    changes according to tutorials

--- web/modules/custom/anytown/src/Plugin/Block/HelloWorldBlock.php
+++ web/modules/custom/anytown/src/Plugin/Block/HelloWorldBlock.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Drupal\anytown\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a hello world block.
@@ -15,15 +19,98 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Custom"),
  * )
  */
-class HelloWorldBlock extends BlockBase
-{
+class HelloWorldBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  private $currentUser;
 
-  public function build(): array
-  {
-    $build['content'] = [
-      '#markup' => $this->t('Hello, World!'),
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private $entityTypeManager;
+
+  /**
+   * Construct a HelloWorldBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    AccountProxyInterface $current_user,
+    EntityTypeManagerInterface $entity_type_manager,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->currentUser = $current_user;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    if ($this->currentUser->isAuthenticated()) {
+      $build['content'] = [
+        '#markup' => $this->t('Hello, @name! Welcome back.', ['@name' => $this->currentUser->getDisplayName()]),
+      ];
+    }
+    else {
+      $build['content'] = [
+        '#markup' => $this->t('Hello, World!'),
+      ];
+    }
+
+    // This block contains content that is different depending on the user so
+    // we don't want it to get cached.
+    // $build['content']['#cache'] = [
+    // 'max-age' => 0,
+    // ];.
+    $build['content']['#cache'] = [
+      // We're creating markup that depends on the current user. So we need
+      // to tell Drupal to use the 'user' cache context. This will ensure that
+      // the block content will vary per-user. Additionally, since we're adding
+      // the user's name to the markup we add a cache tag for the current user.
+      // This will ensure that if the user edits their account and changes their
+      // name that the block will be updated.
+      'contexts' => ['user'],
+      'tags' => $this->entityTypeManager->getStorage('user')->load($this->currentUser->id())->getCacheTags(),
     ];
 
     return $build;
   }
+
 }
